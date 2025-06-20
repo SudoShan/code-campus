a9293a6829548db30a7aa1be6ada3977a43f5b60
BeastShiva <shivaviswanathan07@gmail.com>
2025-06-20 21:12:04 +0530

    bugs resolved

--- client/src/router/AppRouter.tsx
+++ client/src/router/AppRouter.tsx
@@ -4,16 +4,12 @@ import LoginPage from "../pages/LoginPage";
 import SignUpPage from '../pages/SignUpPage';
 import { } from "framer-motion/client";
 import LandingPage from "../pages/LandingPage";
-<<<<<<< HEAD
-import RoomPage from "../pages/RoomPage";
-=======
+import RoomPage from "../pages/RoomPage"
 import AssignmentPage from "../pages/AssignmentPage";
-<<<<<<< HEAD
->>>>>>> eff0d4496816647aadd7a5ef9772664b6408ec9a
 
-=======
+
 import RoomsPage from "../pages/RoomsPage";
->>>>>>> c23c0fb (bugs resolved)
+
 const AppRouter = () => {
   return (
     <Router>
@@ -21,13 +17,13 @@ const AppRouter = () => {
         <Route path="/" element={<LandingPage  />} />
         <Route path="/login" element={<LoginPage  />} />
         <Route path="/signup" element={<SignUpPage />} />
-<<<<<<< HEAD
+
         <Route path="/Dashboard/Room" element={<RoomPage />} />
 =======
         <Route path="/AssignmentPage" element={<AssignmentPage />} />
         <Route path="/rooms" element={<RoomsPage />} />
         
->>>>>>> eff0d4496816647aadd7a5ef9772664b6408ec9a
+
 
         {/* Redirect any unknown routes to Home */}
         <Route path="*" element={<Navigate to="/" replace />} />
